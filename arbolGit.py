256383c1fde487191afbf2f59471d33a75a8868c
Angel Znoker <angel96esti@comunidad.unam.mx>
2017-11-01 19:26:17 -0600

    arbol terminado

--- arbolGit.py
+++ arbolGit.py
@@ -56,30 +56,89 @@ class arbol(object):
 				if n == len(palabra) - 1:
 					palabra[n].significado = sig
 
-
-"""	def agregarPalabra(self, palabra, sig):
-		if palabra[0] not in self.raiz.hijos.keys():
-			palabra = [nodo(n) for n in palabra]
-		else:
-			act = self.raiz.hijos[palabra[0]]
-			palabra[0] = act
-			for n in range(1, len(palabra)):
-				if palabra[n] in act.hijos.keys():
-					palabra[n] = act.hijos[palabra[n]]
-					act = act.hijos[palabra[n].id]
-				else:
-					palabra[n] = nodo(palabra[n])
-					act = palabra[n]
-
-		for n in range(len(palabra)):
-			if palabra[0].id not in self.raiz.hijos.keys():
-				self.agregarRama(self.raiz, palabra[0])
-				palabra[0].agregarPadre(self.raiz)
-			if n != len(palabra) - 1:
-				self.agregarRama(palabra[n], palabra[n + 1])
-				palabra[n + 1].agregarPadre(palabra[n])
-			else:
-				palabra[n].significado = sig"""
-
 	def agregarRama(self, a, b):
 		a.agregarHijo(b)
+
+	def existePalabra(self, act, palabra):
+		if len(palabra) <= 0:
+			if act.significado != "":
+				return act
+			else: return False
+		else:
+			if palabra[0] in act.hijos.keys():
+				act = act.hijos[palabra[0]]
+				return self.existePalabra(act, palabra[1:])
+			else: return False
+
+	def eliminarPalabra(self, palabra):
+		eliminar = self.existePalabra(self.raiz, palabra)
+		if eliminar != False:
+			eliminar.significado = ""
+			act = eliminar
+			for n in range(len(palabra) - 1, -1, -1):
+				if not act.hijos and act.significado == "":
+					del(act.padre.hijos[act.id])
+					act = act.padre
+				else: break
+			return True
+		return False
+
+	def buscarPalabra(self, palabra):
+		encontrado = self.existePalabra(self.raiz, palabra)
+		if encontrado != False:
+			return [''.join(palabra), encontrado.significado]
+		else:
+			return ['Tal vez desee buscar:', self.resultadosRecorrido(palabra)]
+
+	def recorridoInfix(self, act, palabra, lista):
+		if len(act.hijos) > 0:
+			orden = [n for n in act.hijos.keys()]
+			orden.sort()
+			for n in orden:
+				if act.hijos[n].significado != "":
+					lista.append([palabra + n, act.hijos[n].significado])
+					self.recorridoInfix(act.hijos[n], palabra + n, lista)
+				else:
+					self.recorridoInfix(act.hijos[n], palabra + n, lista)
+			return lista
+
+	def resultadosRecorrido(self, palabra):
+		lista = []
+		act = self.raiz
+		con = ""
+		for n in palabra:
+			if n in act.hijos.keys():
+				con += n
+				act = act.hijos[n]
+			else:
+				break
+		lista = self.recorridoInfix(act, con, lista)
+		return lista
+
+	def guardarArchivo(self,ruta):
+		try:
+			archivo = open(ruta,'w')
+			resultado = self.resultadosRecorrido('.')
+			for item in resultado:
+				archivo.write(item[0] + "|" + item[1] + "\n")
+			return("El diccionario ha sido guardado")
+		except IOError as e:
+			return("Error al guardar el diccionario en el archivo")
+		finally:
+			archivo.close()
+
+	def cargaDesdeArchivo(self, ruta):
+		try:
+			archivo = open(ruta,'r')
+			for linea in archivo.readlines():
+				linea = linea.split("|")
+				if(len(linea) == 2):
+					linea[1] = linea[1].replace('\n','').replace('\t','')
+					self.agregarPalabra(list(linea[0].lower()),linea[1])
+				else:
+					return("El archivo no tiene el formato esperado")
+			return("El diccionario se ha cargado exitosamente")
+		except IOError as e:
+			return("Error al cargar el diccionario desde el archivo")
+		finally:
+			archivo.close()
