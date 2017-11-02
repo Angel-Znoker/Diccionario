7c537b8640153ff24acc6d880a274c74fdf6672a
Angel Znoker <angel96esti@comunidad.unam.mx>
2017-11-01 18:12:53 -0600

    termina agregar

--- arbolGit.py
+++ arbolGit.py
@@ -25,4 +25,29 @@ class arbol(object):
 		self.raiz.significado = "raiz"
 
 	def estaVacio(self):
-		
+		if not self.raiz.hijos.keys():
+			return True
+
+	def agregarPalabra(self, palabra, sig):
+		if palabra[0] not in self.raiz.hijos.keys():
+			palabra = [nodo(n) for n in palabra]
+		else:
+			act = self.raiz.hijos[palabra[0]]
+			palabra[0] = act
+			for n in range(1, len(palabra)):
+				if palabra[n] in act.hijos.keys():
+					palabra[n] = act.hijos[palabra[n]]
+					act = act.hijos[palabra[n].id]
+				else:
+					palabra[n] = nodo(palabra[n])
+					act = palabra[n]
+
+		for n in range(len(palabra)):
+			if palabra[0].id not in self.raiz.hijos.keys():
+				self.agregarRama(self.raiz, palabra[0])
+				palabra[0].agregarPadre(self.raiz)
+			if n != len(palabra) - 1:
+				self.agregarRama(palabra[n], palabra[n + 1])
+				palabra[n + 1].agregarPadre(palabra[n])
+			else:
+				palabra[n].significado = sig
